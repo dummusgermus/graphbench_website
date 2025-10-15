f43388d04ec820a5e172902a1019eb18ef076770
erikmuller@t-online.de <erikmuller@t-online.de>
2025-10-15 16:10:13 +0200

    Updates

--- assets/team-BuG8niud.js
+++ assets/team-BuG8niud.js
@@ -1,4 +1,4 @@
-import{r as e}from"./layout-CMBKSRRE.js";const n=document.querySelector("#app");n.innerHTML=e("team",`
+import{r as e,g as n}from"./layout-CMBKSRRE.js";const t=document.querySelector("#app");t.innerHTML=e("team",`
   <section class="page-header">
     <div class="container">
       <div class="title-row">
@@ -16,7 +16,7 @@ import{r as e}from"./layout-CMBKSRRE.js";const n=document.querySelector("#app");
       ${[{name:"Antoine Siraudin",image:"antoine_siraudin.png",institution:"RWTH Aachen University"},{name:"Chendi Qian",image:"chendi_qian.png",institution:"RWTH Aachen University"},{name:"Ben Finkelshtein",image:"ben_finkelshtein.png",institution:"University of Oxford"},{name:"Ali Parviz",image:"ali_parviz.png",institution:"Mila - Quebec AI Institute"},{name:"Darius Weber",image:"darius_weber.png",institution:"RWTH Aachen University"},{name:"Fabrizio Frasca",image:"fabrizio_frasca.png",institution:"Technion – Israel Institute of Technology"},{name:"Hadar Shavit",image:"hadar_shavit.png",institution:"RWTH Aachen University"},{name:"Timo Stoll",image:"timo_stoll.png",institution:"RWTH Aachen University"},{name:"Arman Mielke",image:"arman_mielke.png",institution:"University of Stuttgart"},{name:"Marie Anastacio",image:"marie_anastacio.png",institution:"RWTH Aachen University"},{name:"Erik Müller",image:"erik_muller.png",institution:"RWTH Aachen University"},{name:"Maya Bechler-Speicher",image:"maya_bechler_speicher.png",institution:"Meta"},{name:"Michael M. Bronstein",image:"michael_bronstein.png",institution:"University of Oxford"},{name:"Mikhail Galkin",image:"mikhail_galkin.png",institution:"Google Research"},{name:"Holger H. Hoos",image:"holger_hoos.png",institution:"RWTH Aachen University"},{name:"Mathias Niepert",image:"mathias_niepert.png",institution:"University of Stuttgart"},{name:"Bryan Perozzi",image:"bryan_perozzi.png",institution:"Google Research"},{name:"Jan Tönshoff",image:"jan_tonshoff.png",institution:"Microsoft Research"},{name:"Christopher Morris",image:"christopher_morris.png",institution:"RWTH Aachen University"}].map(i=>`
       <div class="team-item">
         <div class="team-avatar">
-          <img src="/images/team/${i.image}" alt="${i.name}" onerror="this.style.display='none'; this.nextElementSibling.style.display='block';" />
+          <img src="${n()}images/team/${i.image}" alt="${i.name}" onerror="this.style.display='none'; this.nextElementSibling.style.display='block';" />
           <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" style="display: none;"><circle cx="12" cy="8" r="4"/><path d="M6 20a6 6 0 0 1 12 0"/></svg>
         </div>
         <div class="team-name">${i.name}</div>
